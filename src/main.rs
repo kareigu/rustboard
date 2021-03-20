300a50e2f1aaae9fdc41ba3e961dd7571b4bff3b
mxr <eetu.kivelae@gmail.com>
2021-03-20 04:02:36 +0200

    Added index for viewing everyone in the DB along with links to their own pages

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ extern crate rocket;
 
 #[macro_use]
 extern crate dgraph;
+use rocket::response::NamedFile;
 use rocket_contrib::json::Json;
+use rocket_contrib::templates::Template;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
 #[derive(Serialize, Deserialize)]
 struct Data {
@@ -30,12 +33,43 @@ struct Friend {
   age: Option<i32>,
 }
 
-#[get("/<name>")]
-fn index(name: String) -> Json<Vec<FindPerson>> {
+#[derive(Serialize, Deserialize)]
+struct PeopleList {
+  people: Vec<FindPerson>,
+}
+
+#[get("/person/<uid>")]
+fn find(uid: String) -> Template {
+  let person = get_person(uid);
+  Template::render("person", &person[0])
+}
+
+#[get("/")]
+fn index() -> Template {
+  let people = get_people();
+  Template::render("index", people)
+}
+
+#[get("/json/<name>")]
+fn json_find(name: String) -> Json<Vec<FindPerson>> {
+  Json(get_person(name))
+}
+
+#[get("/json/people")]
+fn json_people() -> Json<Vec<FindPerson>> {
+  Json(get_people().people)
+}
+
+#[get("/public/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+  NamedFile::open(Path::new("public/").join(file)).ok()
+}
+
+fn get_person(uid: String) -> Vec<FindPerson> {
   let dgraph = make_dgraph!(dgraph::new_dgraph_client("localhost:9080"));
 
   let q = r#"query find_person($a: string) {
-    find_person(func: eq(name, $a)) {
+    find_person(func: uid($a)) {
       uid
       name
       age
@@ -49,16 +83,43 @@ fn index(name: String) -> Json<Vec<FindPerson>> {
   }"#;
 
   let mut vars = HashMap::new();
-  vars.insert("$a".to_string(), name);
+  vars.insert("$a".to_string(), uid);
 
   let resp = dgraph
     .new_readonly_txn()
     .query_with_vars(q, vars)
     .expect("Dgraph query");
   let data: Data = serde_json::from_slice(&resp.json).expect("parsing");
-  Json(data.find_person)
+  data.find_person
+}
+
+fn get_people() -> PeopleList {
+  let dgraph = make_dgraph!(dgraph::new_dgraph_client("localhost:9080"));
+
+  let q = r#"{
+    find_person(func: type(Person)) {
+        uid
+        name
+        age
+        number_of_friends : count(friend)
+        friend {
+          uid
+          name
+          age
+        }
+    }
+  }"#;
+
+  let resp = dgraph.new_readonly_txn().query(q).expect("Dgraph query");
+  let data: Data = serde_json::from_slice(&resp.json).expect("parsing");
+  PeopleList {
+    people: data.find_person,
+  }
 }
 
 fn main() {
-  rocket::ignite().mount("/", routes![index]).launch();
+  rocket::ignite()
+    .attach(Template::fairing())
+    .mount("/", routes![find, index, files, json_find, json_people])
+    .launch();
 }

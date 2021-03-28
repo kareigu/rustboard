12d0051b8e18404da0c602eeea01d6c39b8779b4
mxr <eetu.kivelae@gmail.com>
2021-03-28 13:24:39 +0300

    Proper error handling for writing files to disk

--- src/api/v1/utils.rs
+++ src/api/v1/utils.rs
@@ -1,27 +1,42 @@
+use std::io::{ErrorKind, Error, Result};
+
 use crate::db::types::Attachment;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use rocket::data::TempFile;
 use regex::Regex;
 
-pub async fn write_attachment(file: &mut TempFile<'_>) -> std::io::Result<Option<Attachment>> {
+pub async fn write_attachment(file: &mut TempFile<'_>) -> Result<Option<Attachment>> {
   if file.len() > 0 {
-    let content_type = file.content_type().unwrap().to_string();
-    let file_slug: String = thread_rng()
-      .sample_iter(&Alphanumeric)
-      .take(14)
-      .map(char::from)
-      .collect();
-    let filename = format!("{}.{}", file_slug, content_type.split("/").last().unwrap(),);
-    let new_attachment = Attachment {
-      filename,
-      content_type,
-    };
-    let path = format!("./files/{}", new_attachment.filename);
-    file.persist_to("/tmp/temp.png").await?;
-    std::fs::copy("/tmp/temp.png", path).expect("Failed copying file");
-    std::fs::remove_file("/tmp/temp.png").expect("Couldn't remove file");
-    Ok(Some(new_attachment))
+    if let Some(c) = file.content_type() {
+      let content_type = c.to_string();
+      let file_slug: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(14)
+        .map(char::from)
+        .collect();
+      let filename = format!("{}.{}", file_slug, content_type.split("/").last().unwrap(),);
+      let new_attachment = Attachment {
+        filename,
+        content_type,
+      };
+
+      match file.persist_to("/tmp/temp.png").await {
+        Ok(_) => {
+          let path = format!("./files/{}", new_attachment.filename);
+          if let Err(e) = std::fs::copy("/tmp/temp.png", path) { Err(e) }
+          else {
+            if let Err(e) = std::fs::remove_file("/tmp/temp.png") { Err(e) }
+            else {
+              Ok(Some(new_attachment))
+            }
+          }
+        },
+        Err(e) => Err(e),
+      }
+    } else {
+      Err(Error::new(ErrorKind::Other, "No content-type"))
+    }
   } else {
     Ok(None)
   }
